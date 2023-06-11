3c9556c31f9501a5d8eafc378432df7de837b1e0
jiwon.An <wldnjs7152@naver.com>
2023-06-11 21:07:43 +0900

    로그인 CSS 마무리 & validation(1차)
    - 로그인 페이지 마크업&CSS 작업완료
    - validation 나머지 작업, api 연동필요

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import imgLogo from "../../assets/imgLogo.svg";
 import "./login.css";
 
+
 export default function Login() {
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+
+    const [idValid, setIdValid] = useState(false);
+    const [pwValid, setPwValid] = useState(false);
+
+    const handleId = (e) => {
+        setId(e.target.value);
+        const regex =
+            /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        if (regex.test(e.target.value)) {
+            setIdValid(true);
+        } else {
+            setIdValid(false);
+        }
+    }
+
+
+    const handlePw = (e) => {
+        setPw(e.target.value);
+        const regex =
+        /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+        if (regex.test(e.target.value)) {
+            setPwValid(true);
+        } else {
+            setPwValid(false);
+        }
+    }
+
     return (
         <div className="loginPageContainer">
             <h1>
@@ -11,14 +41,25 @@ export default function Login() {
             <main className="loginContainer">
                 <div className="formbtnContainer">
                     <button className="buyerForm-btn">구매회원 로그인</button>
-                    <button className="sellerForm-btn">판매회원 로그인</button>
+                    <button disabled={true} className="sellerForm-btn">판매회원 로그인</button>
                 </div>
-
-                <input type='text' placeholder='아이디' className="id-input"></input>
-                <input type='text' placeholder='비밀번호' className="pw-input"></input>
-                <button className="login-btn">로그인</button>
+                <div className="forminputContainer">
+                    <input type='text' placeholder='아이디' value={id} onChange={handleId} className="id-input"></input>
+                    <input type='password' placeholder='비밀번호' value={pw} onChange={handlePw} className="pw-input"></input>
+                    {/* <div className="errormessageWrap">
+                        {!idValid && id.length > 0 && (
+                            <div>비밀번호를 입력해주세요.</div>
+                        )}
+                    </div> */}
+                    <div className="errormessageWrap">
+                        {!pwValid && pw.length > 0 && (
+                            <div>영문, 숫자, 특수기호 포함 8자 이상 입력해주세요.</div>
+                        )}
+                    </div>
+                    <button className="login-btn">로그인</button>
+                </div>
             </main>
-            <footer>
+            <footer className="loginFooter">
                 <a>회원가입</a>
                 <p>|</p>
                 <a>비밀번호 찾기</a>
